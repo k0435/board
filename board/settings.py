3983ad139fc5275421ea31e116743455d43545f3
k0435 <k0435@gcloud.csu.edu.tw>
2023-12-13 09:57:52 +0800

    Update settings.py

--- board/settings.py
+++ board/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'boardapp',
     'captcha',
-     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 CAPTCHA_NOISE_FUNCTIONS = (
@@ -54,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'board.urls'
